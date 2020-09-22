7f4e0d549d16d6b675c6317a13f97330387795e1
wnuke <wnuke@wnuke.dev>
2020-09-21 22:13:05 -0700

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+    application
+}
+group = "dev.wnuke"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/ktor")
+    }
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlinx")
+    }
+}
+dependencies {
+    implementation("io.ktor:ktor-jackson:1.4.0")
+    implementation("io.ktor:ktor-server-netty:1.4.0")
+    implementation("io.ktor:ktor-html-builder:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "14"
+}
+application {
+    mainClassName = "ServerKt"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "hl-mc-kt"
+
