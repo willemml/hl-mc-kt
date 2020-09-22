b6f9dbc5d9b27787343ae3af9c19e65bc1b8f142
wnuke <wnuke@wnuke.dev>
2020-09-22 13:02:27 -0700

    correct Item.kt tag Type

--- src/main/kotlin/protocol/Item.kt
+++ src/main/kotlin/protocol/Item.kt
@@ -2,10 +2,18 @@ package protocol
 
 import io.netty.buffer.ByteBuf
 
-data class Stack constructor(val id: Int = -1, var size: Int = 0, var damage: Int = 0, var tag: Int = 0) {
+data class Stack constructor(var id: Int = -1, var size: Int = 0, var damage: Int = 0, var tag: Tag = Tag(0)) {
     fun write(buffer: ByteBuf) {
         buffer.writeShort(id)
         buffer.writeByte(size)
         buffer.writeShort(damage)
+        tag.writeToBuffer(buffer)
+    }
+
+    fun read(buffer: ByteBuf) {
+        id = buffer.readShort().toInt()
+        size = buffer.readByte().toInt()
+        damage = buffer.readShort().toInt()
+        tag.readFromBuffer(buffer)
     }
 }
